7e817896442daa94e8cd7e1e9a0a0dce429d2ff0
Brad Howes <bradhowes@mac.com>
2020-06-22 16:42:48 +0200

    Cleanup and simplify

--- Shared/Support/Audio/SimplePlayEngine.swift
+++ Shared/Support/Audio/SimplePlayEngine.swift
@@ -12,8 +12,6 @@ final class SimplePlayEngine {
     // The engine's active unit node.
     private var activeAVAudioUnit: AVAudioUnit?
 
-    private var instrumentPlayer: InstrumentPlayer?
-
     // Synchronizes starting/stopping the engine and scheduling file segments.
     private let stateChangeQueue = DispatchQueue(label: "com.example.apple-samplecode.StateChangeQueue")
     
@@ -31,20 +29,6 @@ final class SimplePlayEngine {
     
     // This block will be called every render cycle and will receive MIDI events
     private let midiOutBlock: AUMIDIOutputEventBlock = { sampleTime, cable, length, data in return noErr }
-
-    private var componentType: OSType {
-        return activeAVAudioUnit?.audioComponentDescription.componentType ?? kAudioUnitType_Effect
-    }
-    
-    private var isEffect: Bool {
-        // SimplePlayEngine only supports effects or instruments.
-        // If it's not an instrument, it's an effect
-        return !isInstrument
-    }
-
-    private var isInstrument: Bool {
-        return componentType == kAudioUnitType_MusicDevice
-    }
 
     // MARK: Initialization
 
@@ -94,10 +78,9 @@ extension SimplePlayEngine {
 
         // Destroy the currently connected audio unit, if any.
         if let audioUnit = activeAVAudioUnit {
-            if isEffect {
-                // Break the player -> effect connection.
-                engine.disconnectNodeInput(audioUnit)
-            }
+
+            // Break the player -> effect connection.
+            engine.disconnectNodeInput(audioUnit)
 
             // Break the audio unit -> mixer connection
             engine.disconnectNodeInput(engine.mainMixerNode)
@@ -113,11 +96,8 @@ extension SimplePlayEngine {
 
         // Internal function to resume playing and call the completion handler.
         func rewiringComplete() {
-            if isEffect && isPlaying {
+            if isPlaying {
                 player.play()
-            } else if isInstrument && isPlaying {
-                instrumentPlayer = InstrumentPlayer(audioUnit: activeAVAudioUnit?.auAudioUnit)
-                instrumentPlayer?.play()
             }
             completion()
         }
@@ -128,11 +108,8 @@ extension SimplePlayEngine {
         engine.connect(engine.mainMixerNode, to: engine.outputNode, format: hardwareFormat)
 
         // Pause the player before re-wiring it. It is not simple to keep it playing across an insertion or deletion.
-        if isEffect && isPlaying {
+        if isPlaying {
             player.pause()
-        } else if isInstrument && isPlaying {
-            instrumentPlayer?.stop()
-            instrumentPlayer = nil
         }
 
         guard let avAudioUnit = avAudioUnit else {
@@ -150,19 +127,15 @@ extension SimplePlayEngine {
         // Attach the AVAudioUnit the the graph.
         engine.attach(avAudioUnit)
 
-        if isEffect {
-            // Disconnect the player -> mixer.
-            engine.disconnectNodeInput(engine.mainMixerNode)
+        // Disconnect the player -> mixer.
+        engine.disconnectNodeInput(engine.mainMixerNode)
 
-            // Connect the player -> effect -> mixer.
-            if let format = file?.processingFormat {
-                engine.connect(player, to: avAudioUnit, format: format)
-                engine.connect(avAudioUnit, to: engine.mainMixerNode, format: format)
-            }
-        } else {
-            let stereoFormat = AVAudioFormat(standardFormatWithSampleRate: hardwareFormat.sampleRate, channels: 2)
-            engine.connect(avAudioUnit, to: engine.mainMixerNode, format: stereoFormat)
+        // Connect the player -> effect -> mixer.
+        if let format = file?.processingFormat {
+            engine.connect(player, to: avAudioUnit, format: format)
+            engine.connect(avAudioUnit, to: engine.mainMixerNode, format: format)
         }
+
         rewiringComplete()
     }
 }
@@ -192,185 +165,48 @@ private extension SimplePlayEngine {
     }
 
     func startPlayingInternal() {
-        // assumptions: we are protected by stateChangeQueue. we are not playing.
-        setSessionActive(true)
-        
-        if isEffect {
-            // Schedule buffers on the player.
-            scheduleEffectLoop()
-            scheduleEffectLoop()
+        guard let file = file else {
+            fatalError("`file` must not be nil in \(#function).")
         }
-        
+
+        setSessionActive(true)
+        scheduleEffectLoop(file)
+        scheduleEffectLoop(file)
+
         let hardwareFormat = engine.outputNode.outputFormat(forBus: 0)
         engine.connect(engine.mainMixerNode, to: engine.outputNode, format: hardwareFormat)
         
-        // Start the engine.
         do {
             try engine.start()
         } catch {
             isPlaying = false
             fatalError("Could not start engine. error: \(error).")
         }
-        
-        if isEffect {
-            // Start the player.
-            player.play()
-        } else if isInstrument {
-            instrumentPlayer = InstrumentPlayer(audioUnit: activeAVAudioUnit?.auAudioUnit)
-            instrumentPlayer?.play()
-        }
 
+        player.play()
         isPlaying = true
     }
     
     func stopPlayingInternal() {
-        if isEffect {
-            player.stop()
-        } else if isInstrument {
-            instrumentPlayer?.stop()
-        }
+        player.stop()
         engine.stop()
         isPlaying = false
         setSessionActive(false)
     }
     
-    func scheduleEffectLoop() {
-        guard let file = file else {
-            fatalError("`file` must not be nil in \(#function).")
-        }
-        
+    func scheduleEffectLoop(_ file: AVAudioFile) {
         player.scheduleFile(file, at: nil) {
             self.stateChangeQueue.async {
                 if self.isPlaying {
-                    self.scheduleEffectLoop()
+                    self.scheduleEffectLoop(file)
                 }
             }
         }
     }
 
     func resetAudioLoop() {
-        if isEffect {
-            // Connect player -> mixer.
-            guard let format = file?.processingFormat else { fatalError("No AVAudioFile defined (processing format unavailable).") }
-            engine.connect(player, to: engine.mainMixerNode, format: format)
-        }
-    }
-}
-
-private extension SimplePlayEngine {
-
-    // MARK: InstrumentPlayer
-
-    /// Simple MIDI note generator that plays a two-octave scale.
-    final class InstrumentPlayer {
-
-        private var isPlaying = false
-        private var isDone = false
-        private var noteBlock: AUScheduleMIDIEventBlock
-
-        init?(audioUnit: AUAudioUnit?) {
-            guard let audioUnit = audioUnit else { return nil }
-            guard let theNoteBlock = audioUnit.scheduleMIDIEventBlock else { return nil }
-            noteBlock = theNoteBlock
-        }
-
-        func play() {
-            if !isPlaying {
-                isDone = false
-                scheduleInstrumentLoop()
-            }
-        }
-
-        @discardableResult
-        func stop() -> Bool {
-            isPlaying = false
-            criticalRegion(isDone) {}
-            return isDone
-        }
-
-        private func criticalRegion(_ lock: Any, closure: () -> Void) {
-            defer { objc_sync_exit(lock) }
-            objc_sync_enter(lock)
-            closure()
-        }
-
-        private func scheduleInstrumentLoop() {
-            isPlaying = true
-            DispatchQueue.global(qos: .default).async { self.playingThreead() }
-        }
-
-        private func playingThreead() {
-            let cbytes = UnsafeMutablePointer<UInt8>.allocate(capacity: 3)
-            var step = 0
-
-            // The steps arrays define the musical intervals of a scale (w = whole step, h = half step).
-
-            // C Major: w, w, h, w, w, w, h
-            let steps = [2, 2, 1, 2, 2, 2, 1]
-
-            // C Minor: w, h, w, w, w, h, w
-            // let steps = [2, 1, 2, 2, 2, 1, 2]
-
-            // C Lydian: w, w, w, h, w, w, h
-            // let steps = [2, 2, 2, 1, 2, 2, 1]
-
-            cbytes[0] = 0xB0 // status
-            cbytes[1] = 60 // note
-            cbytes[2] = 0 // velocity
-            self.noteBlock(AUEventSampleTimeImmediate, 0, 3, cbytes)
-
-            usleep(useconds_t(0.5))
-
-            var releaseTime: Float = 0.05
-
-            usleep(useconds_t(0.1 * 1e6))
-
-            var note = 0
-            self.criticalRegion(self.isDone) {
-
-                while self.isPlaying {
-                    // lengthen the releaseTime by 5% each time up to 10 seconds.
-                    if releaseTime < 10.0 {
-                        releaseTime = min(releaseTime * 1.05, 10.0)
-                    }
-
-                    cbytes[0] = 0x90 // note on
-                    cbytes[1] = UInt8(60 + note)
-                    cbytes[2] = 64
-                    self.noteBlock(AUEventSampleTimeImmediate, 0, 3, cbytes)
-
-                    usleep(useconds_t(0.2 * 1e6))
-
-                    cbytes[2] = 0 // note off
-                    self.noteBlock(AUEventSampleTimeImmediate, 0, 3, cbytes)
-
-                    // Reset the note and step after a 2-octave run. (12 semi-tones * 2)
-                    if note >= 24 {
-                        note = 0
-                        step = 0
-                        continue
-                    }
-
-                    // Increment the note interval to the next interval step in the scale
-                    note += steps[step]
-
-                    step += 1
-
-                    if step >= steps.count {
-                        step = 0
-                    }
-
-                } // while isPlaying
-
-                // All notes off
-                cbytes[0] = 0xB0
-                cbytes[1] = 123
-                cbytes[2] = 0
-                self.noteBlock(AUEventSampleTimeImmediate, 0, 3, cbytes)
-                self.isDone = true
-
-                cbytes.deallocate()
-            }
-        }
+        // Connect player -> mixer.
+        guard let format = file?.processingFormat else { fatalError("No AVAudioFile defined (processing format unavailable).") }
+        engine.connect(player, to: engine.mainMixerNode, format: format)
     }
 }
