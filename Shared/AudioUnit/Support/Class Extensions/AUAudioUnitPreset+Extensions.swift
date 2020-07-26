cfcb8d9986ec50cd41f2d55ef7e34a3b530e5b58
Brad Howes <bradhowes@mac.com>
2020-07-26 21:43:15 +0200

    Remove unused class and cleanup

---
+++ Shared/AudioUnit/Support/Class Extensions/AUAudioUnitPreset+Extensions.swift
@@ -0,0 +1,19 @@
+// Changes: Copyright © 2020 Brad Howes. All rights reserved.
+// Original: See LICENSE folder for this sample’s licensing information.
+
+import AVFoundation
+
+internal extension AUAudioUnitPreset {
+
+    /**
+     Initialize new instance with given values
+
+     - parameter number: the unique number for this preset. Factory presets must be non-negative.
+     - parameter name: the display name for the preset.
+     */
+    convenience init(number: Int, name: String) {
+        self.init()
+        self.number = number
+        self.name = name
+    }
+}
