34cc06e67daf58e02e8502caaa68873a5b4e3e4d
Brad Howes <bradhowes@mac.com>
2020-12-30 01:05:12 +0100

    Extension for easier use of FilterParameterAddress

---
+++ Shared/AudioUnit/Support/Class Extensions/AUParameterTree+Extensions.swift
@@ -0,0 +1,9 @@
+// Copyright © 2020 Brad Howes. All rights reserved.
+
+import AVFoundation
+
+public extension AUParameterTree {
+    func parameter(withAddress address: FilterParameterAddress) -> AUParameter? {
+        return parameter(withAddress: address.rawValue)
+    }
+}
