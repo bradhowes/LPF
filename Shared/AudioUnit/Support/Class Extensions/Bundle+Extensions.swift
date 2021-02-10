68510fae463b1978db51a87786a1b4c8225f7031
Brad Howes <bradhowes@mac.com>
2021-02-10 22:06:34 +0100

    Provide getters for AU config properties

--- Shared/AudioUnit/Support/Class Extensions/Bundle+Extensions.swift
+++ Shared/AudioUnit/Support/Class Extensions/Bundle+Extensions.swift
@@ -10,13 +10,13 @@ extension Bundle {
      - parameter key: what to fetch
      - returns: the value found or an empty string
      */
-    private func string(forKey key: String) -> String { infoDictionary?[key] as? String ?? "" }
+    private func info(for key: String) -> String { infoDictionary?[key] as! String }
 
     /// Obtain the release version number associated with the bundle or "" if none found
-    var releaseVersionNumber: String { string(forKey: "CFBundleShortVersionString") }
+    var releaseVersionNumber: String { info(for: "CFBundleShortVersionString") }
 
     /// Obtain the build version number associated with the bundle or "" if none found
-    var buildVersionNumber: String { string(forKey: "CFBundleVersion") }
+    var buildVersionNumber: String { info(for: "CFBundleVersion") }
 
     /// Obtain the bundle identifier or "" if there is not one
     var bundleID: String { Bundle.main.bundleIdentifier?.lowercased() ?? "" }
@@ -31,4 +31,10 @@ extension Bundle {
     /// Obtain a version string with the following format: "Version V.B[ S]"
     /// where V is the releaseVersionNumber, B is the buildVersionNumber and S is the scheme.
     var versionString: String { "Version \(releaseVersionNumber).\(buildVersionNumber)\(scheme)" }
+
+    var auBaseName: String { info(for: "AU_BASE_NAME") }
+    var auComponentName: String { info(for: "AU_COMPONENT_NAME") }
+    var auComponentType: String { info(for: "AU_COMPONENT_TYPE") }
+    var auComponentSubtype: String { info(for: "AU_COMPONENT_SUBTYPE") }
+    var auComponentManufacturer: String { info(for: "AU_COMPONENT_MANUFACTURER") }
 }
