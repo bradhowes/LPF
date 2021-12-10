10d49faea8dab5b7e8a4a1fb73e9e4a6ba1ba09e
Brad Howes <bradhowes@mac.com>
2021-12-10 10:18:40 +0100

    Use right color space

--- Shared/Support/Class Extensions/Color.swift
+++ Shared/Support/Class Extensions/Color.swift
@@ -1,24 +1,33 @@
-// Changes: Copyright © 2020 Brad Howes. All rights reserved.
-// Original: See LICENSE folder for this sample’s licensing information.
+// Copyright © 2021 Brad Howes. All rights reserved.
 
 public extension Color {
   /// Obtain a darker variation of the current color
   var darker: Color {
+#if os(macOS)
+    guard let hsb = usingColorSpace(.extendedSRGB) else { return self }
+#else
+    let hsb = self
+#endif
     var hue: CGFloat = 0
     var saturation: CGFloat = 0
     var brightness: CGFloat = 0
     var alpha: CGFloat = 0
-    getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+    hsb.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
     return Color(hue: hue, saturation: saturation, brightness: brightness * 0.8, alpha: alpha)
   }
 
   /// Obtain a lighter variation of the current color
   var lighter: Color {
+#if os(macOS)
+    guard let hsb = usingColorSpace(.extendedSRGB) else { return self }
+#else
+    let hsb = self
+#endif
     var hue: CGFloat = 0
     var saturation: CGFloat = 0
     var brightness: CGFloat = 0
     var alpha: CGFloat = 0
-    getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+    hsb.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
     return Color(hue: hue, saturation: saturation, brightness: brightness * 1.2, alpha: alpha)
   }
 }
