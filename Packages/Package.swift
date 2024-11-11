3faabc5895b7c24bacc5c38dd5b0bd70661cef15
Brad Howes <bradhowes@mac.com>
2024-11-11 17:14:50 +0100

    Use latest version of AUv3Support

--- Packages/Package.swift
+++ Packages/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .library(name: "UI", targets: ["UI"])
   ],
   dependencies: [
-    .package(url: "https://github.com/bradhowes/AUv3Support", from: "13.0.0"),
+    .package(url: "https://github.com/bradhowes/AUv3Support", exact: "13.0.4"),
     // .package(path: "/Users/howes/src/Mine/AUv3Support")
   ],
   targets: [
